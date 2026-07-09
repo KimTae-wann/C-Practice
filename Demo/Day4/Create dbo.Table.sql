b23ad56c89a7c4bcdb8d26d02ce9efadeea6267c
KimTae-wann <xodhks9711@naver.com>
2026-07-09 17:44:40 +0900

    day4

---
+++ Demo/Day4/Create dbo.Table.sql
@@ -0,0 +1,19 @@
+﻿USE [Test]
+GO
+
+/****** 개체: Table [dbo].[Table] 스크립트 날짜: 2026-07-09 오후 2:25:05 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Member] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [UserId]   NVARCHAR (20) NOT NULL,
+    [Password] NVARCHAR (50) NOT NULL,
+    [Name]     NVARCHAR (50) NOT NULL,
+    [Email]    NVARCHAR (50) NULL
+);
+
+
